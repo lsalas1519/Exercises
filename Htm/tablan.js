a83e3ec553665f19766b6e0554435cdd0167933f
lsalas1519 <50338204+lsalas1519@users.noreply.github.com>
2019-06-12 00:25:23 -0500

    upload

---
+++ Htm/tablan.js
@@ -0,0 +1,4 @@
+var n =prompt('ingrese el número que quiere multiplicar');
+for(i=0; i<10; i++){
+    document.write(n+' * '+(i+1)+' = '+n*(i+1) +"<br>"); 
+}  
---
+++ ModOut.js
@@ -0,0 +1,10 @@
+
+var Module={}
+
+Module.methodOne=function(){
+    console.log('performing a compex task...');
+}
+
+Module.methodTwo=function(){
+    console.log('performing a compex task(2)...');
+}
