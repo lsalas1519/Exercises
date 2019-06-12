07dcaa503fc598d0c6c24c85b0f7141f16a671ff
lsalas1519 <50338204+lsalas1519@users.noreply.github.com>
2019-06-12 01:11:36 -0500

    Delete tempConvertC.js

--- Htm/Events/tempConvertC.js
+++
@@ -1,8 +0,0 @@
-//
-// this may be a class, object, module, or function
-//
-function convert() {
-    let t = document.getElementById("temp").value
-    let o = (t*1.8)+ 32;
-    document.getElementById("out").innerHTML = o.toFixed(2) + " degrees F"
-} 
